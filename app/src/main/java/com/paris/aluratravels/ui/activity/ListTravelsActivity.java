46137583c812ce87f848e2138b8e868a50c45401
renanparis <paris.oliveira@gmail.com>
2019-04-23 12:06:29 -0300

    Inicio projeto.

---
+++ app/src/main/java/com/paris/aluratravels/ui/activity/ListTravelsActivity.java
@@ -0,0 +1,16 @@
+package com.paris.aluratravels.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.paris.aluratravels.R;
+
+public class ListTravelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_travels);
+    }
+}
